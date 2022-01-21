b85da4d13ddec87cdc830eb7d53310918e0940f1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-21 09:00:37 +0530

    if condition

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo "Wrong Input"
+  echo "Available Inputs - frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch"
+fi
+
