43504dcc440f0aa12f38f71d6e07c9a5f4b9d607
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-21 08:32:34 +0530

    Variables topic

--- 05-special-var-inputs.sh
+++ 05-special-var-inputs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo $0
-echo $1
-echo $2
-echo $#
-echo $*
-echo $@
+echo $0 # Script Name
+echo $1 # First Argument
+echo $2 # Second Argument
+echo $# # Number of Arguments
+echo $* # All the arguments
+echo $@ # All the arguments
+
