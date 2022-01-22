8844bd9f8a8937fdcbd3169c11ccc951f110cbe1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-22 07:46:15 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -35,3 +35,8 @@
 echo "Installing NGINX"
 yum install nginx -y >/tmp/roboshop.log
 
+echo "Download Frontend Content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+
+
+
