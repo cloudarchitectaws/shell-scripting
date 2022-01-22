5642ee4b136fb8566490a47bc75e09ab80cd87da
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-22 07:59:44 +0530

    Update shell-scripting/

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -41,6 +41,13 @@ yum install nginx -y &>>$LOG_FILE
 echo "Download Frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 
+echo "Clean Old Content"
+rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+
+echo "Extract Frontend Content"
+cd /usr/share/nginx/html/ &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
+
 
 
 
