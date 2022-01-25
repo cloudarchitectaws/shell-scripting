56d225b0b92dabca05351d57cea0cc0ce6f36fcb
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-25 07:54:34 +0530

    Update shell-scripting/

---
+++ 07-functions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# a set of data to a name is called as variable
+# a set of commands to a name is called as function
+
+## Declare a function
+SAMPLE() {
+  echo "Welcome to DevOps training"
+}
+
+## Call the function
+SAMPLE
