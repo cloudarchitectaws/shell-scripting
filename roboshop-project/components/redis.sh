b3111f023e606bd67aca0af2a2cd4b590d6205c5
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-25 08:23:45 +0530

    Update shell-scripting/

--- roboshop-project/components/redis.sh
+++ roboshop-project/components/redis.sh
@@ -1,24 +1,5 @@
 source components/common.sh
 
-#Redis is used for in-memory data storage and allows users to access the data over API.
-#**Manual Installation of Redis.**
-#1. Install Redis.
-#
-#```bash
-#
-## curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
-## yum install redis -y
-#```
-#
-#2. Update the BindIP from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
-#
-#3. Start Redis Database
-#
-#```bash
-## systemctl enable redis
-## systemctl start redis
-#```
-
 echo "Configuring Redis repo"
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$LOG_FILE
 STAT $?
