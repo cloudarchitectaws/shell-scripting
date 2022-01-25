009d89fdc73009e0748ff8a5de7d65eba1865737
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-25 07:49:47 +0530

    Update shell-scripting/

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]; then
   echo -e "\e[1;31m You should be root user to execute this script ..\e[0m"
   exit
-fi 
+fi
 
 if [ -f components/$1.sh ]; then
   bash components/$1.sh
