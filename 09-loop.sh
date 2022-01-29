b60ce7a0f83e94e1abaf060d14de6431ecd306b0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-29 08:34:45 +0530

    Update shell-scripting/

--- 09-loop.sh
+++ 09-loop.sh
@@ -9,3 +9,10 @@ done
 # for loop is going to iterate number of times is based on number of inputs
 
 # while
+
+# while works on expressions that if condition is using
+i=10
+while [ $i -gt 0 ]; do
+  echo Iteration - $i
+  i=$(($i-1))
+done
