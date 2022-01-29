74888fe1d1b2844822d823f2e9f20d22209d7587
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-29 07:56:01 +0530

    Update shell-scripting/

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -110,5 +110,12 @@ PYTHON() {
   pip3 install -r requirements.txt &>>$LOG_FILE
   STAT $?
 
+  echo "Update Application Config"
+  USER_ID=$(id -u roboshop)
+  GROUP_ID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid = ${USER_ID}" -e "/gid/ c gid = ${GROUP_ID}" /home/roboshop/{{COMPONENT}}/{{COMPONENT}}.ini
+  STAT $?
+
+
   #SYSTEMD_SETUP
 }
