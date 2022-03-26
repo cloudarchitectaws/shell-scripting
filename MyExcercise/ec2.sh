89997296298587f6cff789921cd007b35d1473d7
cloudarchitectaws <cloudarchitectaws@gmail.com>
2022-03-26 10:58:14 +0530

    First

--- MyExcercise/ec2.sh
+++ MyExcercise/ec2.sh
@@ -7,5 +7,5 @@ AMID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pract
 echo $AMID
 
 #Create EC2 instace using the AMIID captured using above command
-aws ec2 run-instances --image-id {$AMID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMID} --instance-type t2.micro
 
