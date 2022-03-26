956f346a5a29d873e17c1301af910f6841c1a9a2
cloudarchitectaws <cloudarchitectaws@gmail.com>
2022-03-26 10:51:16 +0530

    First

--- MyExcercise/ec2.sh
+++ MyExcercise/ec2.sh
@@ -3,6 +3,6 @@
 
 #Finding out AMI ID
 
-AMID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId') | sed -e 's/"//g'
+AMID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMID
 
