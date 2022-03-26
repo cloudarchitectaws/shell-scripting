00d51094ac730c714844e9ca6e1ae3a1f421745a
cloudarchitectaws <cloudarchitectaws@gmail.com>
2022-03-26 11:15:28 +0530

    First

--- MyExcercise/ec2.sh
+++ MyExcercise/ec2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#create EC2 Instance
+#####create EC2 Instance########
+
+#While Running this script input parameter needs to added (for Eg: which machine needs to create eg: cart/user/../../)
+#Checking whether the machine name provided or not if not provided then below piece of code will exit
+#IF first argument $1 is not provided then it will exit with an exit value 1
+
+if [ -z "$1" ]
+then
+  echo "Input Machine Name is needed"
+  exit 1
+fi
+COMPONENT=$1
 
 #Finding out AMI ID
 
@@ -7,5 +18,8 @@ AMID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pract
 echo $AMID
 
 #Create EC2 instace using the AMIID captured using above command
-aws ec2 run-instances --image-id ${AMID} --instance-type t2.micro
+#PROVIDING the TAG specification , fetching the value from $COMPONENT variable
+aws ec2 run-instances --image-id ${AMID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+
+
 
